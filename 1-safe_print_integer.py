0e807265388c2ae6cb584ed28989b4beaafc83ab
Solowise130 <iniodusolomon@gmail.com>
2022-12-19 11:45:09 +0000

    1. Safe printing of an integers list

--- 1-safe_print_integer.py
+++ 1-safe_print_integer.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+safe_print_integer = __import__('1-safe_print_integer').safe_print_integer
 
-def safe_print_integer(value):
-    """Print an integer with "{:d}".format().
-    Args:
-        value (int): The integer to print.
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
-    try:
-        print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
+value = 89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = -89
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
+
+value = "School"
+has_been_print = safe_print_integer(value)
+if not has_been_print:
+    print("{} is not an integer".format(value))
